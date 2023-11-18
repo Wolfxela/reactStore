cf2a500bde2375c4014bcab8edefae328f0119b9
Wolfxela <alexthegreitsas@gmail.com>
2023-11-18 03:24:32 +0200

    finished designing the header

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,50 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import PropTypes from 'prop-types'
 import './App.css'
+function Categories({categories})
+{
+  return(
+  <>
+  {categories.map((category) =>{
+    return(
+    <h3 key={category.name} className="category">
+      {category.name}
+    </h3>)
+  })}
+  </>)
+}
+
 
 function Header()
 {
-  const categories = [{ name:"Foxes", childCategories:["minifoxes","megafoxes"]},{name:"MoreFoxes",childCategories:[]},{name:"EvenMoreFoxes",childCategories:[]},{name:"...Boxes",childCategories:[]}]
+  const categories = [{ name:"Home"},{name:"MoreFoxes"},{name:"MoreFoxes"},{name:"MoreFoxes"},{name:"MoreFoxes"},{name:"MoreFoxes"},{name:"EvenMoreFoxes"},{name:"Filters"}]
   return(
-    <>
       <header>
-        <div className="topPart"></div>
-        <div className="categories">
+
+        <div className="topPart">
+          <h1>ReactStore</h1>
+
+          <label htmlFor="">
+            <input type="search" name="SearchBar" id="search" placeholder='...' />
+          </label>
+          <div className="icons">
+            <img src="" alt="" className="icon" />
+          </div>
 
         </div>
+
+        <nav className="categories">
+          <Categories categories={categories}/>
+        </nav>
       </header>
-    </>
   )
 }
 
 function App() {
-
+ const [money,setMoney] = useState(0)
+ const [searchItem,setSearchItem] = useState("")
   return (
     <>
     <Header/>
@@ -29,4 +54,9 @@ function App() {
   )
 }
 
+Categories.propTypes = {
+
+  categories: PropTypes.array
+  
+}
 export default App
--- src/app.test.jsx
+++ src/app.test.jsx
@@ -1,11 +1,20 @@
 import { describe, it, expect } from 'vitest';
+import App from './App';
+import { render, screen } from '@testing-library/react';
 
-describe('something truthy and falsy', () => {
-  it('true to be true', () => {
-    expect(true).toBe(true);
+describe('test if all categories are displayed right', () => {
+  it('renders all categories', () => {
+    render(<App/>)
+    expect(screen.getByRole('navigation').children.length).toBe(4);
   });
+  it("checks that the order of categories is correct",()=>{
+    render(<App/>)
+    expect(screen.getByRole('navigation').firstChild.textContent).toBe("Home")
+    expect(screen.getByRole('navigation').lastChild.textContent).toBe("Filters")
+  })
+//   it("checks that the multiCategories exist",()=>{
+//     render(<App/>)
+//     expect(screen.getByText("Hello")).toBeInTheDocument()
+//   })
 
-  it('false to be false', () => {
-    expect(false).toBe(false);
-  });
 });
